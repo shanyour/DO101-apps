61348acbde3173106148156b26044da7490cd467
Ahed Alshanyour <shanyour@hotmail.com>
2020-05-10 16:58:54 -0400

    adding zaid message

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/zaid', function (req, res) {
+  res.send('Hello Do you know my name\n My name is Zaid Alshanyour \n do you know my school name \n My school is sinat-Genvieve\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
